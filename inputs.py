d4f828e63cbee7d00d2ed96a76dae009437ab1e5
1aN0rmus <ian.ahl@tekdefense.com>
2013-12-09 20:14:26 -0500

    Update inputs.py
    Docstrings and pep8

--- inputs.py
+++ inputs.py
@@ -1,16 +1,54 @@
+"""
+The inputs.py module represents some form of all inputs
+to the Automater program to include target files, and
+the standard config file - sites.xml. Any addition to
+Automater that brings any other input requirement should
+be programmed in this module.
+
+Class(es):
+TargetFile -- Provides a representation of a file containing target
+              strings for Automater to utilize.
+SitesFile -- Provides a representation of the sites.xml
+             configuration file.
+              
+Function(s):
+No global exportable functions are defined.
+
+Exception(s):
+No exceptions exported.
+"""
 from xml.etree.ElementTree import ElementTree
 import os
 
 class TargetFile(object):
+    """
+    TargetFile provides a Class Method to retrieve information from a file-
+    based target when one is entered as the first parameter to the program.
     
-    @classmethod
-    def FileName(self):
-        return self._filename
+    Public Method(s):
+    (Class Method) TargetList
     
+    Instance variable(s):
+    No instance variables.
+    """
+
     @classmethod
-    def TargetList(self,filename):
+    def TargetList(self, filename):
+        """
+        Opens a file for reading.
+        Returns each string from each line of a single or multi-line file.
+        
+        Argument(s):
+        filename -- string based name of the file that will be retrieved and parsed.
+        
+        Return value(s):
+        Iterator of string(s) found in a single or multi-line file.
+        
+        Restriction(s):
+        This Method is tagged as a Class Method
+        """
         try:
-            target=""
+            target = ""
             with open(filename) as f:
                 li = f.readlines()
                 for i in li:
@@ -18,19 +56,59 @@ class TargetFile(object):
                     yield target
         except IOError:
             print "There was an error reading from the target input file."
-    
+
+
 class SitesFile(object):
+    """
+    SitesFile represents an XML Elementree object representing the
+    program's configuration file. Returns XML Elementree object.
     
+    Method(s):
+    (Class Method) getXMLTree
+    (Class Method) fileExists
+    
+    Instance variable(s):
+    No instance variables.
+    """    
+
     @classmethod
     def getXMLTree(self):
-        try:        
+        """
+        Opens a config file for reading.
+        Returns XML Elementree object representing XML Config file.
+        
+        Argument(s):
+        No arguments are required.
+        
+        Return value(s):
+        ElementTree
+        
+        Restrictions:
+        File must be named sites.xml and must be in same directory as caller.
+        This Method is tagged as a Class Method
+        """
+        try:
             with open("sites.xml") as f:
                 sitetree = ElementTree()
                 sitetree.parse(f)
                 return sitetree
         except:
-            print "There was an error reading from the sites input file. Please check that the xml file is present and correctly formatted."
-                
+            print "There was an error reading from the sites input file.",
+            print "Please check that the XML file is present and correctly formatted."
+
     @classmethod
     def fileExists(self):
+        """
+        Checks if a file exists. Returns boolean representing if file exists.
+        
+        Argument(s):
+        No arguments are required.
+        
+        Return value(s):
+        Boolean
+        
+        Restrictions:
+        File must be named sites.xml and must be in same directory as caller.
+        This Method is tagged as a Class Method
+        """
         return os.path.exists("sites.xml") and os.path.isfile("sites.xml")
